3d22d168937005de0e775ebf70ccd7dd69efbfd4
xparthx <parth.vora.777@gmail.com>
2016-11-04 10:16:19 +0530

    Registration page v1

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
          * Package Service Providers...
          */
 
-        //
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -227,6 +227,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Moloquent' => Jenssegers\Mongodb\Eloquent\Model::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,75 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Register</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+
+                    {!! Form::open(['url' => 'register', 'method' => 'POST', 'files' => TRUE, 
+                    'class' => 'form-horizontal', 'role' => "form"]) !!}
+
+                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <label for="name" class="col-md-4 control-label">Name</label>
+
+                        <div class="col-md-6">
+                        {!! Form::text('name', NULL, ['class' => 'form-control', 'autofocus' => 'autofocus']) !!}
+
+                        @if ($errors->has('name'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                        @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <label for="email" class="col-md-4 control-label">E-mail</label>
+
+                        <div class="col-md-6">
+                        {!! Form::email('email', NULL, ['class' => 'form-control']) !!}
+
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <label for="password" class="col-md-4 control-label">Password</label>
+
+                        <div class="col-md-6">
+                        {!! Form::password('password', ['class' => 'form-control']) !!}
+
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                        <label for="password_confirmation" class="col-md-4 control-label">Repeat password</label>
+
+                        <div class="col-md-6">
+                        {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+
+                        @if ($errors->has('password_confirmation'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password_confirmation') }}</strong>
+                            </span>
+                        @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            {!! Form::submit('Register', ['class' => 'btn btn-primary']) !!}
+                        </div>
+                    </div>
+
+                    {!! Form::close() !!}
+
+                    {{--<form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
@@ -74,7 +142,7 @@
                                 </button>
                             </div>
                         </div>
-                    </form>
+                    </form>--}}
                 </div>
             </div>
         </div>
